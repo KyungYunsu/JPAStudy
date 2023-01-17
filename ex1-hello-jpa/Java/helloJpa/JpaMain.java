4c062e4ebe7440308b93df0648c71a9d9e7c116e
KyungYunsu <kchars82@gmail.com>
2023-01-17 21:55:42 +0900

    JPA study 2장 완료

--- ex1-hello-jpa/Java/helloJpa/JpaMain.java
+++ ex1-hello-jpa/Java/helloJpa/JpaMain.java
@@ -1,18 +1,38 @@
 package helloJpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
 
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-		
-		EntityManager entityManager = emf.createEntityManager();
-		
-		entityManager.close();
-		
+
+		EntityManager em = emf.createEntityManager();
+
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		try {
+//			Member findMember = em.find(Member.class, 1L);
+			List<Member> result = em.createQuery("SELECT m FROM Member as m", Member.class).setFirstResult(0)
+					.setMaxResults(1).getResultList();
+
+			for (Member member : result) {
+				System.out.println(member.getName());
+			}
+
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();// TODO: handle exception
+		} finally {
+			em.close();
+		}
+
 		emf.close();
 	}
 
