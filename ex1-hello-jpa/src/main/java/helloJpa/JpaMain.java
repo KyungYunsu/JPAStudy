119a5ce08394d8f24d5b2235d55d76987192b2ba
KyungYunsu <kchars82@gmail.com>
2023-03-19 14:02:26 +0900

    프록시와 연관관계 관리 cascade와 고아객체 ing

--- ex1-hello-jpa/src/main/java/helloJpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/helloJpa/JpaMain.java
@@ -16,25 +16,14 @@ public class JpaMain {
 		tx.begin();
 
 		try {
-			Team team = new Team();
-			team.setName("teamA");
- 			em.persist(team);
+			Child child1 = new Child();
+			Child child2 = new Child();
+
+			Parent parent = new Parent();
+			parent.addChild(child1);
+			parent.addChild(child2);
 			
-			Member member1 = new Member();
-			member1.setUserName("member1");
-			member1.setTeam(team);
-			em.persist(member1);
-			
-			em.flush(); 
-			em.clear();
-			
-			Member m = em.find(Member.class, member1.getId());
-			
-			System.out.println("m = " + m.getTeam().getClass());
-			
-			System.out.println("============");
-			System.out.println(m.getTeam().getName());
-			System.out.println("============");
+			em.persist(parent);
 			
 			
 			tx.commit();
